f3fb748285751a1af74025eef0bba61289d897ec
Isabel Paredes <iparedes314@gmail.com>
2022-09-07 16:31:29 +0200

    Remove package.json

--- jupyter_rospkg/__init__.py
+++ jupyter_rospkg/__init__.py
@@ -6,18 +6,18 @@ from .handlers import setup_handlers
 
 
 
-HERE = Path(__file__).parent.resolve()
+# HERE = Path(__file__).parent.resolve()
 
 
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
+# with (HERE / "labextension" / "package.json").open() as fid:
+#     data = json.load(fid)
 
 
-def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": data["name"]
-    }]
+# def _jupyter_labextension_paths():
+#     return [{
+#         "src": "labextension",
+#         "dest": data["name"]
+#     }]
 
 
 
