437ad292e46fafe848b9df4eda8ee85e2b101a7c
Isabel Paredes <iparedes314@gmail.com>
2022-09-08 17:47:37 +0200

    Update URL path

--- jupyter_rospkg/tests/test_handlers.py
+++ jupyter_rospkg/tests/test_handlers.py
@@ -3,11 +3,11 @@ import json
 
 async def test_get_example(jp_fetch):
     # When
-    response = await jp_fetch("jupyter-rospkg", "get_example")
+    response = await jp_fetch("ros", "get_example")
 
     # Then
     assert response.code == 200
     payload = json.loads(response.body)
     assert payload == {
-        "data": "This is /jupyter-rospkg/get_example endpoint!"
+        "data": "This is /ros/get_example endpoint!"
     }
